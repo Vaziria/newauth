27bd60a148584ac8564a5e96f910cddaf15263a2
vaziria <manorder123@gmail.com>
2023-01-19 09:37:52 +0700

    mail service test

--- newauth/services/mail_service_test.go
+++ newauth/services/mail_service_test.go
@@ -6,17 +6,26 @@ import (
 
 	"github.com/PDC-Repository/newauth/newauth/services"
 	"github.com/PDC-Repository/newauth/scenario"
+
+	"google.golang.org/appengine/aetest"
 )
 
 func TestSendMail(t *testing.T) {
 	srv := services.NewMailService()
 
 	userScen := scenario.CreateUserScenario()
+	inst, err := aetest.NewInstance(nil)
+
+	if err != nil {
+		t.Fatalf("Failed to create instance: %v", err)
+	}
+
+	defer inst.Close()
 	defer userScen.TearDown()
 
 	user := userScen.User
 
-	req, _ := http.NewRequest(http.MethodPost, "/test", nil)
+	req, _ := inst.NewRequest(http.MethodPut, "/test", nil)
 
 	srv.SendResetEmail(user.Email, "asdasdasdasdasdasdasd", req)
 }
